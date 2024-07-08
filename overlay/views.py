509e3b8718cc809f5b8c99b224414ef76cd6e33f
gamersumit <saggarwal@innow8apps.com>
2024-07-08 14:04:19 +0530

    imports

--- overlay/views.py
+++ overlay/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
-from utils import Mail
 from overlay.service import PDFService, TextOverlay
 
 # Create your views here.
